3241d0142c03b1043dae82a0423efa8ee47c7ded
Dina Gueye <dina3903@gmail.com>
2021-03-25 11:10:37 +0000

    Adding post service, post-form and post-list components ..

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
