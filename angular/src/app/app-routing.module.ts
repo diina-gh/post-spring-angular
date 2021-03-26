2b4c9208371c077a75b40f8305369230839a4d88
Dina Gueye <dina3903@gmail.com>
2021-03-26 11:05:40 +0000

    Configure route for home and contact

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
+import {HomeComponent} from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: AppComponent},
+  { path: 'home', component: HomeComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'form', component: PostFormComponent },
   { path: 'form/:id', component: PostFormComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
