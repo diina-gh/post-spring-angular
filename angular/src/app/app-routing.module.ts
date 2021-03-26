a5a663abdb0566f3ea5e0afce59eb453e16c3686
Dina Gueye <dina3903@gmail.com>
2021-03-26 11:27:12 +0000

    Implement contact page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ContactComponent } from './components/contact/contact.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'form', component: PostFormComponent },
